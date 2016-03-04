493bf2e3b0c7b77715b7dbaedf9ab44d154195d2
DonnyYu <yudongdongcoder@gmail.com>
2016-03-05 00:43:14 +0800

    create GCD file

---
+++ GCDDemo/GCDDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GCDDemo
+//
+//  Created by mac on 16/3/5.
+//  Copyright © 2016年 dongdong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ GCDDemo/GCDDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GCDDemo
+//
+//  Created by mac on 16/3/5.
+//  Copyright © 2016年 dongdong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
